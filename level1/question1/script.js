9bd10896a97ae82487b1f09937884532438641ec
mikkelworld <mikkel@hdlnordic.no>
2019-10-04 22:59:09 +0200

    Level 1 + Level 2

--- level1/question1/script.js
+++ level1/question1/script.js
@@ -1,1 +1,35 @@
 //Use RegEx to validate form
+function validateForm(event) {
+	// Prevents form from submitting. Allows us to test first and submit through JS after validation.	
+	event.preventDefault();
+
+	// Get the form
+	var contactForm = document.forms["contactForm"];
+	// Get the field values
+	var firstName = contactForm["firstname"].value;
+	var lastName = contactForm["lastname"].value;
+	var phone = contactForm["phone"].value;
+	var email = contactForm["email"].value;	
+	// Test phone and email against regex patterns
+	var isPhoneValid = validatePhone(phone);
+	var isEmailValid = validateEmail(email);
+
+	// Note: We could also use regex for testing the first and last names, but since the assignment only asks for us to test that they arent empty, we will simply look at their length. 
+	// This could also be achieved in html with the required attribute.
+
+	// Make sure all criteria are met
+	if (firstName.length > 0 && lastName.length > 0 && isPhoneValid && isEmailValid) {
+		alert("The form is valid. We should probably call form.submit() now.");
+		// At this point we could call contactForm.submit() in order to properly submit the form.
+	}
+}
+
+function validateEmail(input) {
+	var pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+	return pattern.test(input);
+}
+
+function validatePhone(input) {
+	var pattern = /^[0-9]{8}$/;
+	return pattern.test(input);
+}
--- level1/question2/script.js
+++ level1/question2/script.js
@@ -1,1 +1,17 @@
 //Console.log out elements in JSON file
+var data = "{\"video\": [{\"id\": 12312412312, \"name\": \"Ecuaciones Diferenciales\", \"url\": \"/video/math/edo/12312412312\", \"author\": { \"data\": [{ \"name_author\": \"Alejandro Morales\", \"url\": \"/author/alejandro-morales\", \"type\": \"master\" }]}}]}";
+
+var json = JSON.parse(data);
+
+for (var i = 0; i < json.video.length; i++) {
+	console.log(json.video[i].id);
+	console.log(json.video[i].name);
+	console.log(json.video[i].url);
+	console.log(json.video[i].author);
+
+	for (var j = 0; j < json.video[i].author.data.length; j++) {
+		console.log(json.video[i].author.data[j].name_author);
+		console.log(json.video[i].author.data[j].url);
+		console.log(json.video[i].author.data[j].type);
+	}
+}
